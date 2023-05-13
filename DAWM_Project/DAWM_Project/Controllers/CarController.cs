0d9f6449b6b3f20b1e09c139bdce82c38350318c
Andrei Oprea <andreidei0406@yahoo.com>
2023-05-13 13:56:11 +0300

    added filter functionality

--- DAWM_Project/DAWM_Project/Controllers/CarController.cs
+++ DAWM_Project/DAWM_Project/Controllers/CarController.cs
@@ -1,4 +1,5 @@
-﻿using DAWM_Project.Services;
+﻿using DAWM_Project.Data.Entity;
+using DAWM_Project.Services;
 using DAWM_Project.Services.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
@@ -51,6 +52,31 @@ namespace DAWM_Project.Controllers
             var rez = _carService.GetUserCars(userId);
 
             return Ok(rez);
+        }
+
+        [HttpGet("/getFilteredCars")]
+        public IActionResult GetFilteredCars(string maker="all", string modelName="all", 
+            int minYear=0, int maxYear=10000, int minPower=0, int maxPower=2000,
+            int minEngineCapacity=0, int maxEngineCapacity=6000,
+            int minMileage=0, int maxMileage=1000000,
+            float minPrice=0, float maxPrice=10000000,
+            bool? negociable = null)
+        {
+            var rez = _carService.GetAll();
+
+            var rezFiltered = new List<Car>();
+
+            foreach(var car in rez)
+            {
+                if((car.Maker == maker || maker =="all" || maker =="") && (car.ModelName == modelName || modelName == "all" || modelName == "" || car.ModelName.ToLower().Contains(modelName.ToLower())) && (car.ModelYear >= minYear && car.ModelYear <= maxYear) 
+                    && (car.Power >= minPower && car.Power <= maxPower) && (car.EngineCapacity >= minEngineCapacity && car.EngineCapacity <= maxEngineCapacity)
+                    && (car.Mileage >= minMileage && car.Mileage <= maxMileage) && (car.Price >= minPrice && car.Price <= maxPrice) && (car.Negotiable == negociable || negociable == null))
+                {
+                    rezFiltered.Add(car);
+                }
+            } 
+
+            return Ok(rezFiltered);
         }
     }
 }
